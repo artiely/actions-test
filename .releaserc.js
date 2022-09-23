7ef4ccc85e23398cdc5768f67effacacf5ae367c
artiely <1119696785@qq.com>
2022-09-23 16:23:16 +0800

    feat: new branch

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports={
-    "branch": "main",
+    "branch": "master",
     "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
